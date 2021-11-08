3f9d322b3241815696e80eebfd8731862c3711e9
Luca Orlandini <luca.orlandini@gmail.com>
2021-11-08 17:47:15 +0100

    add Dashboards GeoStories Application Contexts

--- js/appConfig.js
+++ js/appConfig.js
@@ -35,6 +35,68 @@ export default {
         name: "mapviewer",
         path: "/viewer/:mapId",
         component: require('@mapstore/product/pages/MapViewer').default
+    }, {
+        name: 'context',
+        path: "/context/:contextName",
+        component: require('@mapstore/product/pages/Context').default
+    }, {
+        name: 'context',
+        path: "/context/:contextName/:mapId",
+        component: require('@mapstore/product/pages/Context').default
+    }, {
+        name: 'context-creator',
+        path: "/context-creator/:contextId",
+        component: require('@mapstore/product/pages/ContextCreator').default
+    }, {
+        name: "manager",
+        path: "/manager",
+        component: require('@mapstore/product/pages/Manager').default
+    }, {
+        name: "manager",
+        path: "/manager/:tool",
+        component: require('@mapstore/product/pages/Manager').default
+    }, {
+        name: "context-manager",
+        path: "/context-manager",
+        component: require('@mapstore/product/pages/pages/ContextManager').default
+    }, {
+        name: "dashboard",
+        path: "/dashboard",
+        component: require('@mapstore/product/pages/Dashboard').default
+    }, {
+        name: "dashboard",
+        path: "/dashboard/:did",
+        component: require('@mapstore/product/pages/Dashboard').default
+    }, {
+        name: "rulesmanager",
+        path: "/rules-manager",
+        component: require('@mapstore/product/pages/RulesManager').default
+    }, {
+        name: "geostory",
+        path: "/geostory/:gid",
+        component: require('@mapstore/product/pages/GeoStory').default
+    }, {
+        name: "geostory",
+        path: "/geostory/:gid/section/:sectionId",
+        component: require('@mapstore/product/pages/GeoStory').default
+    }, {
+        name: "geostory",
+        path: "/geostory/:gid/section/:sectionId/column/:columnId",
+        component: require('@mapstore/product/pages/GeoStory').default
+    }, {
+        name: "geostory",
+        path: "/geostory/shared/:gid",
+        component: require('@mapstore/product/pages/GeoStory').default
+    },
+    {
+        name: "geostory",
+        path: "/geostory/shared/:gid/section/:sectionId",
+        component: require('@mapstore/product/pages/GeoStory').default
+    },
+    {
+        name: "geostory",
+        path: "/geostory/shared/:gid/section/:sectionId/column/:columnId",
+        component: require('@mapstore/product/pages/GeoStory').default
     }],
     pluginsDef,
     initialState: {
